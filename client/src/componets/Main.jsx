e31a5f36cc72ff57d966b807be2163f170a8c022
tinashetrishsamupindi <tinashetrishsamupindi@gmail.com>
2024-06-04 21:11:59 -0700

    modified motto statement

--- client/src/componets/Main.jsx
+++ client/src/componets/Main.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Main() {
   return (
     <div className='flex justify-center items-center'>
-        <h1 className='text-3xl font-serif text-white'> Welcome to Interior Designs</h1>
+        <h1 className='text-3xl font-serif text-white'> Designs as Unique as you are</h1>
     </div>
   )
 }
